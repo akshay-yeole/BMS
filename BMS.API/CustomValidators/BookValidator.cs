77f4f2dd4c56348cf26407e90f98de5a57fd8a5a
akshay-yeole <akshayyeole1@outlook.com>
2024-04-07 13:07:42 +0530

    validations

---
+++ BMS.API/CustomValidators/BookValidator.cs
@@ -0,0 +1,28 @@
+﻿using BMS.Domain.Constants;
+using BMS.Domain.Dto;
+using FluentValidation;
+
+namespace BMS.API.CustomValidators
+{
+    public class BookValidator : AbstractValidator<BookDto>
+    {
+        public BookValidator()
+        {
+            RuleFor(r => r.CopiesAvailable)
+                .GreaterThanOrEqualTo(3)
+                .WithMessage("Available copies for the book should be at least 3.");
+
+            RuleFor(r => r.BookName)
+                .NotEmpty()
+                .WithMessage("Book name cannot be empty.");
+
+            RuleFor(r => r.Author)
+                .NotEmpty()
+                .WithMessage("Author name cannot be empty.");
+            
+            RuleFor(r => r.Categoryid)
+                .NotEmpty()
+                .WithMessage("Category ID cannot be empty.");
+        }
+    }
+}
---
+++ BMS.API/CustomValidators/StudentModelValidator.cs
@@ -0,0 +1,25 @@
+﻿using BMS.Domain.Dto;
+using FluentValidation;
+
+namespace BMS.API.CustomValidators
+{
+    public class StudentModelValidator : AbstractValidator<StudentDto>
+    {
+        public StudentModelValidator()
+        {
+
+            RuleFor(model => model.Std)
+                .GreaterThan(3)
+                .WithMessage("Standard for student should be between 1-10");
+
+            RuleFor(model => model.Div)
+                .NotEmpty().WithMessage("Division should not be empty.")
+                .Must(div => div == 'A' || div == 'B' || div == 'C')
+                .WithMessage("Division should be 'A', 'B', or 'C'.");
+
+            RuleFor(model => model.RollNo)
+                .GreaterThan(0).WithMessage("Roll number should be greater than 0.")
+                .InclusiveBetween(1, 50).WithMessage("Roll number should be between 1 and 50.");
+        }
+    }
+}
